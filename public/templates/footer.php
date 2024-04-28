d692e69e74aae3c975babd929d38d5d76bb8b00c
AnnaK126 <145970421+AnnaK126@users.noreply.github.com>
2024-04-28 12:00:55 +0100

    Footer
    Fixed broken footer links

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -3,10 +3,10 @@
 <div class="container">
     <footer class="py-3 my-4">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Stay</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Dine</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Gallery</a></li>
+            <li class="nav-item"><a href="index.php" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="stays.php" class="nav-link px-2 text-body-secondary">Stay</a></li>
+            <li class="nav-item"><a href="dine.php" class="nav-link px-2 text-body-secondary">Dine</a></li>
+            <li class="nav-item"><a href="gallery.php" class="nav-link px-2 text-body-secondary">Gallery</a></li>
 <!--            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"></a></li>-->
         </ul>
         <p class="text-center text-body-secondary">© Hotel Tallafornia</p>
